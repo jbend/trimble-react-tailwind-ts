13b633d5d0f334b84c334e7e52a31947c29d82a8
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2022-05-06 10:07:11 -0700

    Setting up help layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-// import './App.css';
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <Content />
+      <Footer />
+    </>
   );
 }
 
